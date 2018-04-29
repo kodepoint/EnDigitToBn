8d903a016dcfd2759c41eae244f548e860cce102
kodepoint <33001275+kodepoint@users.noreply.github.com>
2018-04-29 20:31:13 +0600

    Create enToBn.cs

---
+++ enToBn.cs
@@ -0,0 +1,13 @@
+public string cnDgt(string input)
+        {
+            return input.Replace('0', '০')
+                .Replace('1', '১')
+                .Replace('2', '২')
+                .Replace('3', '৩')
+                .Replace('4', '৪')
+                .Replace('5', '৫')
+                .Replace('6', '৬')
+                .Replace('7', '৭')
+                .Replace('8', '৮')
+                .Replace('9', '৯');
+        }
